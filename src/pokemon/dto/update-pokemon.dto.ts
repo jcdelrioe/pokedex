5600ec806deba4c025d3d24424e9f6c50b0b1e9b
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-02 12:34:26 -0500

    Conect Pokemon in DB

--- src/pokemon/dto/update-pokemon.dto.ts
+++ src/pokemon/dto/update-pokemon.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePokemonDto } from './create-pokemon.dto';
+import { PartialType } from '@nestjs/mapped-types'
+import { CreatePokemonDto } from './create-pokemon.dto'
 
 export class UpdatePokemonDto extends PartialType(CreatePokemonDto) {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePokemonDto } from './dto/create-pokemon.dto';
-import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common'
+
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { Pokemon } from './entities/pokemon.entity'
+
+import { CreatePokemonDto } from './dto/create-pokemon.dto'
+import { UpdatePokemonDto } from './dto/update-pokemon.dto'
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase()
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto)
+
+      return pokemon
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `Pokemon exist in DB ${JSON.stringify(error.keyValue)}`,
+        )
+      }
+      console.log(error)
+      throw new InternalServerErrorException(
+        `Can't create pokemon - Check server logs`,
+      )
+    }
   }
 
   findAll() {
-    return `This action returns all pokemon`;
+    return `This action returns all pokemon`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+    return `This action returns a #${id} pokemon`
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+    return `This action updates a #${id} pokemon`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+    return `This action removes a #${id} pokemon`
   }
 }
