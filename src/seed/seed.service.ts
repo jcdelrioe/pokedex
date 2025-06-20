9e3bd4f985b2648a9dedc801460b317e59b9b9fe
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-20 17:43:48 -0500

    Antes de insertar por lote

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common'
+import { PokeResponse } from './interfaces/poke.response.interface'
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed'
+  private readonly fetch: typeof fetch
+  async executeSeed() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    if (!response.ok) {
+      throw new Error('Failed to fetch Pokémon data')
+    }
+    const data = (await response.json()) as PokeResponse
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/')
+      const no: number = +segments[segments.length - 2]
+
+      console.log({ name, no })
+    })
+
+    return data.results
   }
 }
