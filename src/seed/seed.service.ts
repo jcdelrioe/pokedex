63637a8e4580b2a095cfb1d47d4ed7465f9219dc
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-20 18:06:49 -0500

    Insert lot

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common'
 import { PokeResponse } from './interfaces/poke.response.interface'
+import { InjectModel } from '@nestjs/mongoose'
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity'
+import { Model } from 'mongoose'
 
 @Injectable()
 export class SeedService {
   private readonly fetch: typeof fetch
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
     if (!response.ok) {
@@ -11,13 +20,13 @@ export class SeedService {
     }
     const data = (await response.json()) as PokeResponse
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/')
       const no: number = +segments[segments.length - 2]
 
-      console.log({ name, no })
+      const pokemon = await this.pokemonModel.create({ name, no })
     })
 
-    return data.results
+    return 'Seed executed successfully'
   }
 }
