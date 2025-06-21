839a8edabb99f02dc13554d24799fe7d1b78734f
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-21 11:20:49 -0500

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
 import { join } from 'path'
 import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
-import { PokemonModule } from './pokemon/pokemon.module'
 import { MongooseModule } from '@nestjs/mongoose'
-import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config'
+
+import { PokemonModule } from './pokemon/pokemon.module'
+import { CommonModule } from './common/common.module'
+import { SeedModule } from './seed/seed.module'
+import { EnvConfiguration } from './config/env.config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(
+      process.env.MONGODB ??
+        (() => {
+          throw new Error('MONGODB environment variable is not defined')
+        })(),
+    ),
     PokemonModule,
     CommonModule,
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
---
+++ src/config/env.config.ts
@@ -0,0 +1,13 @@
+interface EnvConfig {
+  environment: string
+  mongodb: string | undefined
+  port: string | number
+  defaultLimit: string | number
+}
+
+export const EnvConfiguration: () => EnvConfig = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGODB,
+  port: process.env.PORT || 3002,
+  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+})
